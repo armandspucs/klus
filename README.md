65c09e9bade3e2a9d7f635f6a9350862e853ded2
Armands Pucs <armandspucs@gmail.com>
2020-09-18 21:34:36 +0000

    a

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Skat. 'brančus' un atrodi dažādas pabeigtības pakāpes...
 
 bonus:
 - repl.it konfigurācijas fails
+- wireframe ko augustā zīmējām https://docs.google.com/drawings/d/1SD0BlwuWukdRMusgcn8t4_pBQXGCnXCO-RlniyZnS4s/edit?usp=sharing
--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-fetch('sample.json')
-  .then(response => response.json())
-  .then(data => console.log(data));
-
-
 const vielasPoga = document.getElementById('vielas-poga');
 const aprikojumsPoga = document.getElementById('aprikojums-poga');
 const aprikojumsRindas = document.getElementsByClassName('aprikojums');
@@ -23,3 +18,91 @@ aprikojumsPoga.addEventListener('click', function() {
         vielasRindas[i].classList.toggle('slepts');
     }
 });
+
+
+
+
+
+
+
+
+async function raditNoliktavasDatus()
+{
+  let datiNoServera = await fetch('noliktava.json');
+  let datiJson;
+
+  if (datiNoServera.ok) 
+  {
+    datiJson = await datiNoServera.json();
+  }
+
+  //let datiJson = await dati.json();
+  let datiNoliktava = datiJson['noliktava'];
+  
+  let tabula = document.getElementById('public-db-table-body');
+  tabula.innerHTML = '';
+
+  //datiNoliktava.length - saņemto ierakstu skaits
+  //datiNoliktava['0'] - pirmais ieraksts
+  //datiNoliktava['0']['Nosaukums'] - pirmā ieraksta nosaukums
+
+  for (i = 0; i < datiNoliktava.length; i++)
+  {
+    //i - loop indekss 0,1,2,3,4....
+
+    tipsClass = datiNoliktava[i]['Tips'];
+    tipsClass = tipsClass.toLowerCase();
+    tipsClass = tipsClass.replace('ī','i');
+
+    tabula.innerHTML = tabula.innerHTML+`
+    <tr class="`+tipsClass+`">
+    <td> `+datiNoliktava[i]['ID']+` </td>
+    <td> `+datiNoliktava[i]['Nosaukums']+` </td>
+    <td> `+datiNoliktava[i]['Tips']+` </td>
+    <td> `+datiNoliktava[i]['Apakštips']+` </td>
+    <td> `+datiNoliktava[i]['Skaits']+` </td>
+    <td> `+datiNoliktava[i]['Svars']+` </td>
+    <td> `+datiNoliktava[i]['Komentāri']+` </td>
+    </tr>`;
+
+
+  }//loop beigas
+
+
+}//beidzas raditNoliktavasDatus(dati)
+
+raditNoliktavasDatus();
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////
+////////////////////////////////////
+//////////SNIPPETS BELOW ///////////
+
+//fetch('sample.json')
+//  .then(response => response.json())
+//  .then(data => console.log(data));
+
+/*
+async function ieladetDatus(adrese) 
+{
+  fetch(adrese).then(sanemtDatus);
+}
+
+ieladetDatus('noliktava.json');
+
+
+async function sanemtDatus(dati)
+{
+  //console.log(dati);
+  //datiOutput = await dati.json();
+  console.log( await dati.json() );
+}
+*/
